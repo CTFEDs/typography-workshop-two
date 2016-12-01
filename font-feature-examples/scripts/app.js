4be99e6eac14cd3f7bc92a53fd341b9a0b17b703
JustinSlack <justin@newmedialabs.co.za>
2016-12-01 21:47:17 +0200

    Typo fix

--- font-feature-examples/scripts/app.js
+++ font-feature-examples/scripts/app.js
@@ -17,3 +17,24 @@ var toggleButtons = document.querySelectorAll(".btn-primary.toggle"),
 for (var i = 0; i < toggleButtonsLength; i += 1) {
   toggleButtons[i].addEventListener("click", function() { toggleExample(this) });
 }
+/*! ctfeds-typography-workshop 01-12-2016 */
+for(var toggleExample=function(a){var b=a.previousElementSibling,c=a.getAttribute("data-feature");-1===b.className.indexOf(c)?(b.classList.add(c),a.innerHTML="revert"):(b.classList.remove(c),a.innerHTML="enable")},toggleButtons=document.querySelectorAll(".btn-primary.toggle"),toggleButtonsLength=toggleButtons.length,i=0;toggleButtonsLength>i;i+=1)toggleButtons[i].addEventListener("click",function(){toggleExample(this)});
+var toggleExample = function(currentButton) {
+  var example = currentButton.previousElementSibling,
+      feature = currentButton.getAttribute('data-feature');
+
+  if (example.className.indexOf(feature) === -1) {
+    example.classList.add(feature);
+    currentButton.innerHTML = "revert";
+  } else {
+    example.classList.remove(feature);
+    currentButton.innerHTML = "enable";
+  }
+}
+
+var toggleButtons = document.querySelectorAll(".btn-primary.toggle"),
+    toggleButtonsLength = toggleButtons.length;
+
+for (var i = 0; i < toggleButtonsLength; i += 1) {
+  toggleButtons[i].addEventListener("click", function() { toggleExample(this) });
+}
